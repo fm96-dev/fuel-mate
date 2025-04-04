2cee5894db5b2ce25f8759b0fefc4506d519c99a
Federico Mameli <federico_mameli@yahoo.com>
2025-04-04 10:01:58 +0000

    feat: add environment variables for Distance Matrix and OpenCageData APIs

--- env.d.ts
+++ env.d.ts
@@ -1,2 +1,12 @@
 /// <reference types="vite/client" />
 /// <reference types="unplugin-vue-router/client" />
+
+interface ImportMetaEnv {
+  readonly VITE_DISTANCE_MATRIX_BASE_URL: string
+  readonly VITE_DISTANCE_MATRIX_API_KEY: string
+  readonly VITE_OPENCAGEDATA_API_KEY: string
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
