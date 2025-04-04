473052cd37199122f88f3ab615746a48d96e27a5
Federico Mameli <federico_mameli@yahoo.com>
2025-04-04 10:10:55 +0000

    chore(vite): add @vitejs/plugin-basic-ssl for local development

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,17 @@ import vue from '@vitejs/plugin-vue'
 import router from 'unplugin-vue-router/vite'
 import { VueRouterAutoImports } from 'unplugin-vue-router'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import ssl from '@vitejs/plugin-basic-ssl'
 import ui from '@nuxt/ui/vite'
 import uiConfig from './ui.config'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    ssl({
+      name: 'dev',
+      domains: ['localhost', '*.local'],
+    }),
     router({
       dts: 'router.d.ts',
       routesFolder: 'src/views',
