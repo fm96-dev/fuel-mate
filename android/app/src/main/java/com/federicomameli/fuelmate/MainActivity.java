af142f386d221889eea65d948301f4cc7bc93548
Federico Mameli <f.mameli@playmoove.com>
2025-05-12 16:28:09 +0200

    feat: add capacitor and android

---
+++ android/app/src/main/java/com/federicomameli/fuelmate/MainActivity.java
@@ -0,0 +1,5 @@
+package com.federicomameli.fuelmate;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.federicomameli.fuelmate',
+  appName: 'FuelMate',
+  webDir: 'dist'
+};
+
+export default config;
