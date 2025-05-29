9f957be50305e27a2dd08e0baa682b063693605a
Federico Mameli <federico_mameli@yahoo.com>
2025-05-29 23:50:20 +0200

    fix: update viewport meta tag and title in index.html; wrap app in UseScreenSafeArea in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,17 @@
 <template>
-  <UApp>
-    <Navbar />
-    <RouterView />
-  </UApp>
+  <UseScreenSafeArea top right bottom left>
+    <UApp>
+      <Navbar />
+      <RouterView />
+    </UApp>
+  </UseScreenSafeArea>
 </template>
+
+<style lang="postcss">
+#app {
+  padding-top: var(--vueuse-safe-area-top);
+  padding-left: var(--vueuse-safe-area-left);
+  padding-right: var(--vueuse-safe-area-right);
+  padding-bottom: var(--vueuse-safe-area-bottom);
+}
+</style>
